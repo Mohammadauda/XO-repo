8ad6a8dad9a050466ef4e6facb0f291ab73976fd
Mohammadauda <wi25b128@technikum-wien.at>
2026-06-28 13:29:31 +0200

    ci.yml_debuge

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -2,11 +2,6 @@ package org;
 
 public class Main {
     public static void main(String[] args) {
-       //Diesen code hat einen ERROR, möchte CI/DC yml datei testen
-        int zahl = "Hallo";
-        System.out.println(zahl);
-       //ende der ci/CD error code
-
 
         TicTacToe game = new TicTacToe();
         game.start();
